edb85b6d214e84000ee1ec39ef43ce70e1b57f18
prabhakar <undurthi.prabhakar@gmail.com>
2013-12-04 14:53:43 +0530

    Final commit.

--- index.php
+++ index.php
@@ -7,39 +7,37 @@ $recent = get_products_recent();
 $pageTitle = "Unique T-shirts designed by a frog";
 $section = "home";
 include(ROOT_PATH . 'inc/header.php'); ?>
-		<div class="section banner">
+        <div class="section banner">
 
-			<div class="wrapper">
+            <div class="wrapper">
 
-				<img class="hero" src="<?php echo BASE_URL; ?>img/mike-the-frog.png" alt="Mike the Frog says:">
-				<div class="button">
-					<a href="<?php echo BASE_URL; ?>shirts.php">
-						<h2>Hey, I&rsquo;m Mike!</h2>
-						<p>Check Out My Shirts</p>
-					</a>
-				</div>
-			</div>
+                <img class="hero" src="<?php echo BASE_URL; ?>img/mike-the-frog.png" alt="Mike the Frog says:">
+                <div class="button">
+                    <a href="<?php echo BASE_URL; ?>shirts.php">
+                        <h2>Hey, I&rsquo;m Mike!</h2>
+                        <p>Check Out My Shirts</p>
+                    </a>
+                </div>
+            </div>
 
-		</div>
+        </div>
 
-		<div class="section shirts latest">
+        <div class="section shirts latest">
 
-			<div class="wrapper">
+            <div class="wrapper">
 
-				<h2>Mike&rsquo;s Latest Shirts</h2>
+                <h2>Mike&rsquo;s Latest Shirts</h2>
 
-				<ul class="products">
-					<?php
-						$list_view_html = "";
-						foreach($recent as $product) { 
-							$list_view_html = get_list_view_html($product) . $list_view_html;
-						}
-						echo $list_view_html;
-					?>								
-				</ul>
+                <ul class="products">
+                    <?php
+                        foreach(array_reverse($recent) as $product) {
+                            include(ROOT_PATH . "inc/partial-product-list-view.html.php");
+                        }
+                    ?>
+                </ul>
 
-			</div>
+            </div>
 
-		</div>
+        </div>
 
 <?php include(ROOT_PATH . 'inc/footer.php') ?>
